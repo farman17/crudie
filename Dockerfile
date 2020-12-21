5a4708b6c97b7b26cf81cefbadccf6b90a6b13ab
farman17 <ahmadfarman1701@gmail.com>
2020-12-21 09:51:43 +0700

    melakukan perubahan pada file..

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM php:7.2-apache
-
-RUN docker-php-ext-install pdo pdo_mysql mysqli
-
-WORKDIR /var/www/html
-
-COPY . /var/www/html/ 
--- table.sql
+++
@@ -1,2 +0,0 @@
-oci runtime error: exec failed: container_linux.go:265: starting container process caused "exec: \"/usr/bin/mysqldump\": stat /usr/bin/mysqldump: no such file or directory"
-
